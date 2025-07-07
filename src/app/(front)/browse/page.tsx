f6c81c6d82b1d27fd7d372b38619fd8482f68699
Sandesh TCM <s.mankar@thecodingmachine.com>
2025-07-07 23:38:48 +0100

    basic changes

--- src/app/(front)/browse/page.tsx
+++ src/app/(front)/browse/page.tsx
@@ -1,9 +1,11 @@
 import { Suspense } from "react";
 import BrowserInner from "@/components/forms/BrowserInner";
+import Loading from "@/components/layout/Loading";
+import { LoadingForm } from "@/components/layout";
 
 export default function CreateProfile() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<LoadingForm />}>
       <BrowserInner />
     </Suspense>
   );
