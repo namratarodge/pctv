c7d6cd32d0363504d974bd9f002c62afdc8c354e
Sandesh TCM <s.mankar@thecodingmachine.com>
2025-07-19 13:54:13 +0100

    fixed

--- src/components/layout/TopicSlider.tsx
+++ src/components/layout/TopicSlider.tsx
@@ -1,4 +1,3 @@
-// components/VoicesSlider.js
 "use client";
 
 import { useEffect } from "react";
@@ -10,11 +9,8 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { usePublicData } from "../context/PublicDataContext";
 
 import { TagType } from "@/constants/Type";
-import {
-  FaceSmileIcon,
-} from '@heroicons/react/24/outline';
-
-
+import { FaceSmileIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
 
 type VoicesSliderProps = {
   title: string;
@@ -25,7 +21,7 @@ type VoicesSliderProps = {
 export default function TopicSlider({ title }: VoicesSliderProps) {
   const { categories } = usePublicData();
   useEffect(() => {
-    console.log('new')
+    console.log("new");
     console.log(categories);
   }, [categories]);
 
@@ -47,18 +43,20 @@ export default function TopicSlider({ title }: VoicesSliderProps) {
         }}
         className="relative"
       >
-        {categories.map((item : TagType, index : number) => {
+        {categories.map((item: TagType, index: number) => {
           return (
-          <SwiperSlide key={index}>
-            <div
-              className={`rounded-xl p-3 gap-2 shadow-lg flex flex-col bg-gray-900 h-45 `}
-            >
-              <div className="bg-gray-800 p-4 rounded-xl items-center text-center">
-                <FaceSmileIcon className="h-20 w-20 text-white mx-auto" />
-              </div>
-              <h2 className="text-center text-xs">{item.display_name}</h2>
-            </div>
-          </SwiperSlide> );
+            <SwiperSlide key={index}>
+              <Link
+                href={`browse?genre=${item.name}`}
+                className={`rounded-xl p-3 gap-2 shadow-lg flex flex-col bg-gray-900 h-45 `}
+              >
+                <div className="bg-gray-800 p-4 rounded-xl items-center text-center">
+                  <FaceSmileIcon className="h-20 w-20 text-white mx-auto" />
+                </div>
+                <h2 className="text-center text-xs">{item.display_name}</h2>
+              </Link>
+            </SwiperSlide>
+          );
         })}
       </Swiper>
     </div>
