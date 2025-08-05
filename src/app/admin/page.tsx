9f15961facc83dad8365986a1db79eb676c7caeb
Sandesh TCM <s.mankar@thecodingmachine.com>
2025-08-05 23:18:26 +0100

    admin home

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -6,36 +6,6 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 
-const stats = [
-  {
-    name: "Total Title",
-    stat: "71,897",
-    previousStat: "70,946",
-    change: "12%",
-    changeType: "increase",
-  },
-  {
-    name: "Total People",
-    stat: "58.16%",
-    previousStat: "56.14%",
-    change: "2.02%",
-    changeType: "increase",
-  },
-  {
-    name: "Total Video",
-    stat: "24.57%",
-    previousStat: "28.62%",
-    change: "4.05%",
-    changeType: "decrease",
-  },
-  {
-    name: "Review",
-    stat: "24.57%",
-    previousStat: "28.62%",
-    change: "4.05%",
-    changeType: "decrease",
-  },
-];
 
 function classNames(...classes: (string | false | null | undefined)[]): string {
   return classes.filter(Boolean).join(" ");
