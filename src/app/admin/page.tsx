a832c0005e6ced3f521140bd77b38d1652d51c0a
Sandesh TCM <s.mankar@thecodingmachine.com>
2025-08-05 23:16:07 +0100

    admin home

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,10 @@
+"use client";
+
 import HomeCharts from "@/components/forms/HomeChart";
 import { ArrowDownIcon, ArrowUpIcon } from "@heroicons/react/20/solid";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 
 const stats = [
   {
@@ -36,18 +41,60 @@ function classNames(...classes: (string | false | null | undefined)[]): string {
   return classes.filter(Boolean).join(" ");
 }
 
-export default function Example() {
+type StatItemRaw = {
+  name: string;
+  stat: number;
+  previousStat: number;
+  change: number;
+  changeType: "increase" | "decrease";
+};
+
+export default function Home() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetch = async () => {
+    const token = localStorage.getItem("token");
+    setLoading(true);
+    try {
+      const response = await axios.get(
+        `${process.env.NEXT_PUBLIC_API_URL}/admin/home`,
+        {
+          headers: {
+            Authorization: token,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (response.data.status) {
+        const modifiedData = response.data.data;
+        console.log(modifiedData);
+        setLoading(false);
+        setData(modifiedData);
+      }
+    } catch (error) {
+      console.log(error);
+      toast("Error fetching data:");
+    }
+  };
+
+  useEffect(() => {
+    fetch();
+  }, []);
+
   return (
     <div>
-      <h3 className="text-base font-semibold text-gray-900">Welcome, ADMIN Back!</h3>
-      <dl className="mt-5 grid grid-cols-1 gap-1 divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow-sm md:grid-cols-4 md:divide-x md:divide-y-0">
-        {stats.map((item) => (
-          <div key={item.name} className="px-4 py-5 sm:p-6">
+      <h3 className="text-base font-semibold text-gray-900">
+        Welcome, ADMIN Back!
+      </h3>
+      <dl className="mt-5 grid grid-cols-1 gap-1 divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow-sm md:grid-cols-5 md:divide-x md:divide-y-0">
+        {data.map((item: StatItemRaw) => (
+          <div key={item.name} className="px-2 py-2 sm:p-5">
             <dt className="text-base font-normal text-gray-900">{item.name}</dt>
             <dd className="mt-1 flex items-baseline justify-between md:block lg:flex">
-              <div className="flex items-baseline text-2xl font-semibold text-red-600">
+              <div className="flex items-baseline text-xl font-semibold text-red-600">
                 {item.stat}
-                <span className="ml-2 text-sm font-medium text-gray-500">
+                <span className="ml-2 text-xs font-medium text-gray-500">
                   from {item.previousStat}
                 </span>
               </div>
@@ -57,7 +104,7 @@ export default function Example() {
                   item.changeType === "increase"
                     ? "bg-green-100 text-green-800"
                     : "bg-red-100 text-red-800",
-                  "inline-flex items-baseline rounded-full px-2.5 py-0.5 text-sm font-medium md:mt-2 lg:mt-0"
+                  "inline-flex items-baseline rounded-full px-2 py-0.5 text-sm font-medium md:mt-2 lg:mt-0"
                 )}
               >
                 {item.changeType === "increase" ? (
