ae7f9bf13db9cdee69a7e540896307be0e786522
Sandesh TCM <s.mankar@thecodingmachine.com>
2025-08-07 23:51:05 +0100

    fixed issue

--- src/constants/Validation.tsx
+++ src/constants/Validation.tsx
@@ -14,7 +14,7 @@ export const userUpdateSchema = z.object({
   email: z.string().min(1, "email is required"),
   gender: z.string().min(1, "Gender is required"),
   country: z.string().min(1, "Country is required"),
-  phone: z.number().min(1, "Phone is required")
+  phone: z.string().min(1, "Phone is required")
 });
 export type UserUpdateFormData = z.infer<typeof userUpdateSchema>;
 
