ee41fd6579137426d1703837db653f6062c320f5
Sandesh TCM <s.mankar@thecodingmachine.com>
2025-09-02 18:09:43 +0100

    fixed

--- src/components/account/membership.tsx
+++ src/components/account/membership.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import {
-  AdjustmentsHorizontalIcon
-} from "@heroicons/react/24/outline";
+import { AdjustmentsHorizontalIcon } from "@heroicons/react/24/outline";
+import axios from "axios";
+import { usePublicData } from "../context/PublicDataContext";
 import ListButton from "./ListButton";
 import MyMembership from "./MyMembership";
 
@@ -15,6 +15,36 @@ export const accountSettingsLinks = [
 ];
 
 export default function Membership() {
+  const { user } = usePublicData();
+
+  const subscription = user?.subscription;
+
+  const handleSubscribe = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      if (!token) {
+        window.location.href = "/login";
+      }
+
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_API_URL}/create-portal-session`,
+        {
+          customerId: subscription.customer_id,
+        },
+        {
+          headers: {
+            Authorization: token,
+          },
+        }
+      );
+      if (response.data) {
+        window.location.href = response.data.url; // redirect to billing portal if user is already subscribed
+      }
+    } catch (error) {
+      console.error("Subscription error", error);
+    }
+  };
+
   return (
     <>
       <div className="min-h-screen bg-white text-black max-w-3xl mx-auto ">
@@ -28,9 +58,15 @@ export default function Membership() {
             <ListButton item={item} key={item.name} />
           ))}
         </div>
-        <button className="rounded-full text-red-400 border hover:bg-red-500 hover:text-white border-red-400 px-4 py-2 mt-4 text-sm cursor-pointer">
-          Cancel Membership
-        </button>
+
+        {subscription.customer_id && (
+          <button
+            onClick={() => handleSubscribe()}
+            className="rounded-full text-red-400 border hover:bg-red-500 hover:text-white border-red-400 px-4 py-2 mt-4 text-sm cursor-pointer"
+          >
+            Cancel Membership
+          </button>
+        )}
       </div>
     </>
   );
